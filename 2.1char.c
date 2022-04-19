9b1ed26597d183b80fc7e44dcb1548183738b195
羅仲宏 <chunghung@hungs-MacBook-Pro.local>
2022-04-19 23:14:26 +0800

    first commit

---
+++ 2.1char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// 輸入大寫字母，輸出小寫
+// char 是整數型別運算,用單引號包住一個符號：'a'
+// ascii 中Ａ:65 a:97
+int main()
+{
+    char input, output;
+    printf("enter uppercase :");
+    scanf("%c", &input);
+    // output = input + 32;
+    output = input + ('a' - 'A');
+    printf("%c\n", output);
+}
