1ffefcde766eee24f8fc3f89edaec3128112b0b7
joshchunghung <joshchunghung@gmail.com>
2022-12-01 17:02:29 +0800

    1201

---
+++ 22.2swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+利用 pointer 交換 a b 的值
+*/
+void swap(int *,int *);
+
+int main(){
+    int a=3,b=5;
+    //swap(a,b); // copy a b 到 swap 中，不會真正改a b 值
+    swap(&a,&b);
+    printf("a=%d,b=%d\n",a,b);
+    return 0;
+}
+
+void swap(int *a,int *b){  //tips: 參數加* { } 內的變數也要加*
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
